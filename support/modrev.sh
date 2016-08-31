80fb587284575c6925a4344a2867301c7ba085a2
Akinobu Lee <ri@nitech.ac.jp>
2016-08-31 09:51:59 +0900

    fix compilation error on some systems

--- support/modrev.sh
+++ support/modrev.sh
@@ -3,11 +3,20 @@
 # change version
 # (should be executed in the parent directory)
 #
-newver=$1
+newver_major=$1
+newver_minor=$2
 
-for i in ./libjulius/configure.in ./support/build-all.sh; do
+newver=${newver_major}.${newver_minor}
+
+for i in ./support/build-all.sh; do
 	echo $i
 	sed -e "s/^JULIUS_VERSION=[^ ]*/JULIUS_VERSION=$newver/" < $i > tmp
+	diff $i tmp
+	mv tmp ${i}
+done
+for i in ./libjulius/configure.in ; do
+	echo $i
+	cat $i | sed -e "s/^JULIUS_MAJOR_VERSION=[^ ]*/JULIUS_MAJOR_VERSION=$newver_major/" | sed -e "s/^JULIUS_MINOR_VERSION=[^ ]*/JULIUS_MINOR_VERSION=$newver_minor/"  > tmp
 	diff $i tmp
 	mv tmp ${i}
 done
