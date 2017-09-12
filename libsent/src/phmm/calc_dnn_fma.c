6eef538e2bd96074cb5df035dd7b8eceffefa7c3
Akinobu Lee <ri@nitech.ac.jp>
2017-09-12 18:47:54 +0900

    faster loop on DNN/fma

--- libsent/src/phmm/calc_dnn_fma.c
+++ libsent/src/phmm/calc_dnn_fma.c
@@ -24,19 +24,82 @@ calc_dnn_fma(float *dst, float *src, float *w, float *b, int out, int in, float 
   int i, j;
 
   int n = in / 8;
-  for (i = 0; i < out; i++) {
-    __m256 x = _mm256_setzero_ps();
-    s = src;
-    for (j = 0; j < n; j++) {
-      __m256 v1 = _mm256_load_ps(w);
-      __m256 v2 = _mm256_load_ps(s);
-      x = _mm256_fmadd_ps(v1, v2, x);
-      w += 8;
-      s += 8;
-    }
-    _mm256_store_ps(fstore, x);
-    *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
-  }
 
+  if (out % 4 == 0) {
+    /* batch 4 */
+    float *w2, *w3, *w4;
+    for (i = 0; i < out; i += 4) {
+      __m256 x1 = _mm256_setzero_ps();
+      __m256 x2 = _mm256_setzero_ps();
+      __m256 x3 = _mm256_setzero_ps();
+      __m256 x4 = _mm256_setzero_ps();
+      w2 = w + in;
+      w3 = w2 + in;
+      w4 = w3 + in;
+      s = src;
+      for (j = 0; j < n; j++) {
+	__m256 vs = _mm256_load_ps(s);
+	x1 = _mm256_fmadd_ps(vs, _mm256_load_ps(w), x1);
+	x2 = _mm256_fmadd_ps(vs, _mm256_load_ps(w2), x2);
+	x3 = _mm256_fmadd_ps(vs, _mm256_load_ps(w3), x3);
+	x4 = _mm256_fmadd_ps(vs, _mm256_load_ps(w4), x4);
+	s  += 8;
+	w  += 8;
+	w2 += 8;
+	w3 += 8;
+	w4 += 8;
+      }
+      _mm256_store_ps(fstore, x1);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x2);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x3);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x4);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      w = w4;
+    }
+  } else if (out % 2 == 0) {
+    /* batch 2 */
+    float *w2;
+    for (i = 0; i < out; i += 2) {
+      __m256 x1 = _mm256_setzero_ps();
+      __m256 x2 = _mm256_setzero_ps();
+      s = src;
+      w2 = w + in;
+      for (j = 0; j < n; j++) {
+	__m256 vs = _mm256_load_ps(s);
+	__m256 v1 = _mm256_load_ps(w);
+	__m256 v2 = _mm256_load_ps(w2);
+	x1 = _mm256_fmadd_ps(vs, v1, x1);
+	x2 = _mm256_fmadd_ps(vs, v2, x2);
+	s  += 8;
+	w  += 8;
+	w2 += 8;
+      }
+      _mm256_store_ps(fstore, x1);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x2);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      w = w2;
+    }
+
+  } else {
+    /* no batch */
+    for (i = 0; i < out; i++) {
+      __m256 x = _mm256_setzero_ps();
+      s = src;
+      for (j = 0; j < n; j++) {
+	__m256 vs = _mm256_load_ps(s);
+	__m256 v = _mm256_load_ps(w);
+	x = _mm256_fmadd_ps(vs, v, x);
+	s  += 8;
+	w  += 8;
+      }
+      _mm256_store_ps(fstore, x);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+    }
+  }
+  
 #endif	/* HAS_SIMD_FMA */
 }
