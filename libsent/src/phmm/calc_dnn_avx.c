5b856fa0746d98ecb4845f49f7fb0f2b1e831223
Akinobu Lee <ri@nitech.ac.jp>
2017-09-13 13:25:44 +0900

    more SIMD optimization considering latency

--- libsent/src/phmm/calc_dnn_avx.c
+++ libsent/src/phmm/calc_dnn_avx.c
@@ -24,19 +24,91 @@ calc_dnn_avx(float *dst, float *src, float *w, float *b, int out, int in, float 
   int i, j;
 
   int n = in / 8;
-  for (i = 0; i < out; i++) {
-    __m256 x = _mm256_setzero_ps();
-    s = src;
-    for (j = 0; j < n; j++) {
-      __m256 v1 = _mm256_load_ps(w);
-      __m256 v2 = _mm256_load_ps(s);
-      v2 = _mm256_mul_ps(v1, v2);
-      x = _mm256_add_ps(x, v2);
-      w += 8;
-      s += 8;
+
+  if (out % 4 == 0) {
+    /* batch 4 */
+    float *w2, *w3, *w4;
+    for (i = 0; i < out; i += 4) {
+      __m256 x1 = _mm256_setzero_ps();
+      __m256 x2 = _mm256_setzero_ps();
+      __m256 x3 = _mm256_setzero_ps();
+      __m256 x4 = _mm256_setzero_ps();
+      s = src;
+      w2 = w + in;
+      w3 = w2 + in;
+      w4 = w3 + in;
+      for (j = 0; j < n; j++) {
+	__m256 vs = _mm256_load_ps(s);
+	__m256 v1 = _mm256_load_ps(w);
+	__m256 v2 = _mm256_load_ps(w2);
+	__m256 v3 = _mm256_load_ps(w3);
+	__m256 v4 = _mm256_load_ps(w4);
+	v1 = _mm256_mul_ps(vs, v1);
+	v2 = _mm256_mul_ps(vs, v2);
+	v3 = _mm256_mul_ps(vs, v3);
+	v4 = _mm256_mul_ps(vs, v4);
+	x1 = _mm256_add_ps(x1, v1);
+	x2 = _mm256_add_ps(x2, v2);
+	x3 = _mm256_add_ps(x3, v3);
+	x4 = _mm256_add_ps(x4, v4);
+	s  += 8;
+	w  += 8;
+	w2 += 8;
+	w3 += 8;
+	w4 += 8;
+      }
+      _mm256_store_ps(fstore, x1);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x2);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x3);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x4);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      w = w4;
     }
-    _mm256_store_ps(fstore, x);
-    *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+  } else if (out % 2 == 0) {
+    /* batch 2 */
+    float *w2;
+    for (i = 0; i < out; i += 2) {
+      __m256 x1 = _mm256_setzero_ps();
+      __m256 x2 = _mm256_setzero_ps();
+      s = src;
+      w2 = w + in;
+      for (j = 0; j < n; j++) {
+	__m256 vs = _mm256_load_ps(s);
+	__m256 v1 = _mm256_load_ps(w);
+	__m256 v2 = _mm256_load_ps(w2);
+	v1 = _mm256_mul_ps(vs, v1);
+	v2 = _mm256_mul_ps(vs, v2);
+	x1 = _mm256_add_ps(x1, v1);
+	x2 = _mm256_add_ps(x2, v2);
+	s  += 8;
+	w  += 8;
+	w2 += 8;
+      }
+      _mm256_store_ps(fstore, x1);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      _mm256_store_ps(fstore, x2);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+      w = w2;
+    }
+  } else {
+    /* no batch */
+    for (i = 0; i < out; i++) {
+      __m256 x = _mm256_setzero_ps();
+      s = src;
+      for (j = 0; j < n; j++) {
+	__m256 vs = _mm256_load_ps(w);
+	__m256 v1 = _mm256_load_ps(s);
+	v1 = _mm256_mul_ps(vs, v1);
+	x = _mm256_add_ps(x, v1);
+	w += 8;
+	s += 8;
+      }
+      _mm256_store_ps(fstore, x);
+      *(dst++) = fstore[0] + fstore[1] + fstore[2] + fstore[3] + fstore[4] + fstore[5] + fstore[6] + fstore[7] + *(b++);
+    }
   }
 
 #endif	/* HAS_SIMD_AVX */
