06529cc6004ed029ba73278d2c229ee592a4e020
Akinobu Lee <ri@nitech.ac.jp>
2020-09-13 16:37:33 +0900

    updated to handle options and generate forward DFA

--- gramtools/mkdfa/mkdfa.py
+++ gramtools/mkdfa/mkdfa.py
@@ -8,18 +8,20 @@ Implementation of mkdfa.pl for python 3
 import sys
 import os
 import shutil
-import tempfile
 import re
 import codecs
 import subprocess
+import argparse
 
 JULIUS_BIN = os.path.dirname(os.path.abspath(__file__))
 if sys.platform == 'win32':
     CMD_MKFA = JULIUS_BIN + '/mkfa.exe'
     CMD_DFA_MINIMIZE = JULIUS_BIN + '/dfa_minimize.exe'
+    CMD_DFA_DETERMINIZE = JULIUS_BIN + '/dfa_determinize.exe'
 else:
     CMD_MKFA = JULIUS_BIN + '/mkfa'
     CMD_DFA_MINIMIZE = JULIUS_BIN + '/dfa_minimize'
+    CMD_DFA_DETERMINIZE = JULIUS_BIN + '/dfa_determinize'
 
 # generate reverse grammar file
 def gen_reverse_grammar(gramfile, rgramfile):
@@ -107,33 +109,71 @@ def call_mkfa(rgramfile, tmpvocafile, dfafile, tmpprefix):
     subprocess.run(cmd)
 
     print("---")
-    if os.path.exists(CMD_DFA_MINIMIZE):
-        cmd = [CMD_DFA_MINIMIZE, tmpprefix + '.dfa', '-o', dfafile]
-        subprocess.run(cmd)
-    else:
-        print("Warning: dfa_minimize not found in the same place as mkdfa.py")
-        print("Warning: no minimization performed")
-        shutil.copyfile(tmpprefix + '.dfa', dfafile)
+    cmd = [CMD_DFA_MINIMIZE, tmpprefix + '.dfa', '-o', dfafile]
+    subprocess.run(cmd)
 
     os.unlink(tmpprefix + '.dfa')
     os.unlink(tmpprefix + '.h')
 
+def make_forward_dfa(dfafile, fdfafile, tmpprefix):
+
+    fdfafile_nfa = fdfafile + '_nfa'
+    fdfafile_beforeminimize = fdfafile + '_beforeminimize'
+
+    endstates = {}
+    dfamax = -1;
+    with codecs.open(dfafile, "r", 'utf-8') as fin:
+        for line in fin:
+            elem = list(map(int, line.split()))
+            if (elem[1] == -1 and elem[2] == -1):
+                endstates[elem[0]] = 1
+            if (dfamax < elem[0]):
+                dfamax = elem[0]
+    dfamax += 1
+    with codecs.open(fdfafile_nfa, "w", 'utf-8') as fout:
+        print("%d -1 -1 1 0" % (dfamax), file=fout)
+        with codecs.open(dfafile, "r", 'utf-8') as fin:
+            for line in fin:
+                elem = list(map(int, line.split()))
+                if (elem[1] == -1 and elem[2] == -1):
+                    continue
+                if (elem[0] == 0):
+                    elem[0] = dfamax
+                if (elem[2] == 0):
+                    elem[2] = dfamax
+                if (elem[0] != dfamax and endstates.get(elem[0])):
+                    elem[0] = 0
+                if (elem[2] != dfamax and endstates.get(elem[2])):
+                    elem[2] = 0
+                print("%d %d %d 0 0" % (elem[2], elem[1], elem[0]), file=fout)
+
+    cmd = [CMD_DFA_DETERMINIZE, fdfafile_nfa, '-o', fdfafile_beforeminimize]
+    subprocess.run(cmd)
+
+    cmd = [CMD_DFA_MINIMIZE, fdfafile_beforeminimize, '-o', fdfafile]
+    subprocess.run(cmd)
+
+    os.unlink(fdfafile_nfa)
+    os.unlink(fdfafile_beforeminimize)
+
+
 if __name__ == '__main__':
 
-    if len(sys.argv) < 2:
-        print("mkdfa.py --- DFA compiler for python 3")
-        print("usage: mkdfa.py [-n] prefix")
-        print("\t-n ... keep current dict, not generate")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prefix", help = "grammar file prefix")
+    parser.add_argument("-n", help = "keep current dict, not generate", action="store_true")
+    parser.add_argument("-r", help = "not generate forward grammar", action="store_true")
+    args = parser.parse_args()
+
+    flag = False;
+    for p in [CMD_MKFA, CMD_DFA_MINIMIZE, CMD_DFA_DETERMINIZE]:
+        if (os.path.exists(p) == False):
+            print("Error: %s not found" % (p), file=sys.stderr)
+            flag = True
+    if (flag):
         sys.exit(1)
 
-    if (os.path.exists(CMD_MKFA) == False):
-        print("Error: \"mkfa\" not found on the same directory of mkdfa.py")
-        sys.exit(1)
-    if (os.path.exists(CMD_DFA_MINIMIZE) == False):
-        print("Warning: \"dfa_minimize\" not found in the same directory as mkdfa.py")
-        print("Warning: no minimization performed")
-
-    corename = sys.argv[1]
+    corename = args.prefix
 
     gramfile = corename + ".grammar"
     vocafile = corename + ".voca"
@@ -142,9 +182,11 @@ if __name__ == '__main__':
     termfile = corename + ".term"
     fdfafile = corename + ".dfa.forward"
 
-    tmpprefix   = tempfile.gettempdir() + "/_julius_mkdfa_tmp_"
+    tmpprefix   = "./_julius_mkdfa_tmp_"
     tmpvocafile = tmpprefix + ".voca"
     rgramfile = tmpprefix + ".grammar"
+
+    generated = ""
 
     # generate reverse grammar file
     gen_reverse_grammar(gramfile, rgramfile)
@@ -156,14 +198,20 @@ if __name__ == '__main__':
 
     # call mkfa to generate .dfa from reversed grammar (and minimize) 
     call_mkfa(rgramfile, tmpvocafile, dfafile, tmpprefix)
-    
-    # call mkfa to generate .dfa_forward from original grammar (and minimize) 
-    call_mkfa(gramfile, tmpvocafile, fdfafile, tmpprefix)
 
-    vocafile2dictfile(vocafile, dictfile)
+    generated = dfafile + " " + termfile
+    
+    # generate .dfa_forward from original grammar
+    if (args.r == False):
+        make_forward_dfa(dfafile, fdfafile, tmpprefix)
+        generated += " " + fdfafile
+
+    if (args.n == False):
+        vocafile2dictfile(vocafile, dictfile)
+        generated += " " + dictfile
 
     print('---')
-    print("generated %s %s %s %s" % (dfafile, termfile, dictfile, fdfafile))
+    print("generated " + generated)
 
     os.unlink(tmpvocafile)
     os.unlink(rgramfile)
